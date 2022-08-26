b3dbc292c55f52a106c303f7d32c0f09d5fd6400
shivam-garg7 <shivamgarg2232001@gmail.com>
2022-08-26 18:27:11 +0530

    commit

--- day15/game1.js
+++ day15/game1.js
@@ -1,4 +1,10 @@
 function loading(){
-    document.getElementById('btn1').style.visibility="hidden"
-
+    document.getElementById('container').style.visibility="hidden"
+    document.getElementById('loading').style.visibility="visible"
+    document.getElementById('loading').style.display="block"
+    setTimeout(() => {
+       
+        document.getElementById('loading').style.visibility="hidden"
+        document.getElementById('loading').style.display="none"
+    }, 3000);
 }
